84ab027709c3aa8cd5c2b0a3d5c98ed56fdc0d16
Lancelot de Ferrière <wraitii@gmail.com>
2024-05-18 14:50:13 +0200

    Update to full-state HyleOutput

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -9,22 +9,25 @@ risc0_zkvm::guest::entry!(main);
 pub fn main() {
     let input: HyleInput<u32> = env::read();
 
+    let initial_state = u32::from_be_bytes(input.initial_state.clone().try_into().unwrap());
     env::commit(&HyleOutput {
+        version: 1,
         block_number: input.block_number,
         block_time: input.block_time,
+        sender: input.sender,
         caller: input.caller,
         tx_hash: input.tx_hash,
-        program_outputs: Some("Any output heehee"),
-        initial_state: u32::to_be_bytes(input.initial_state).to_vec(),
+        program_outputs: "Any output heehee",
+        initial_state: input.initial_state,
         next_state: u32::to_be_bytes(
-            if input.initial_state == 1 {
-                match input.program_inputs.unwrap() {
+            if initial_state == 1 {
+                match input.program_inputs {
                     0 => panic!("Cannot reset to 0 as that would block the contract."),
                     a => a
                 }
             } else {
                 // Calculate the next number in the collatz conjecture
-                let mut n = input.initial_state;
+                let mut n = initial_state;
                 if n % 2 == 0 {
                     n = n / 2;
                 } else {
